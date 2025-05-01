ff97a0ec4b318cf4042035eb49b729c7ebe65099
Xintaro <dmitrolutianskiy@gmail.com>
2025-05-01 13:29:05 +0300

    11

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello World!\n"
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
